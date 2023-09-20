052029bb6380b70b396b333d799017dfb477736c
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-09-20 18:09:22 +0500

    Auto-Service project created and committed

---
+++ src/main/java/com/company/autoservice/AutoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.autoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/autoservice/AutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.autoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
