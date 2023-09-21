f6a02972813b2e19151d559bc676e20132b076ec
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-09-21 12:48:07 +0500

    entities and enums created

---
+++ src/main/java/com/company/autoservice/entity/Car.java
@@ -0,0 +1,39 @@
+package com.company.autoservice.entity;
+
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity(name = "car")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Car extends BaseEntity{
+
+    @Column(nullable = false, unique = true)
+    private String VIN;
+
+    @Column(nullable = false)
+    private String maker;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private String bodyType;
+
+    @Column(nullable = false)
+    private String fuelType;
+
+    @Column(nullable = false)
+    private Integer horsepower;
+
+    @Column(nullable = false)
+    private Integer CMM;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+}
---
+++ src/main/java/com/company/autoservice/entity/Company.java
@@ -0,0 +1,60 @@
+package com.company.autoservice.entity;
+
+
+import com.company.autoservice.enums.Status;
+import lombok.*;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "company")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Company extends BaseEntity{
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private Address address;
+
+    @Column(nullable = false)
+    private String contactPerson;
+
+    @Column(nullable = false, unique = true)
+    private String contactPhone;
+
+    private Long credits;
+
+    private Long availableTokens;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<User> employees;
+
+}
+
+@Entity(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+class Address extends BaseEntity{
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String house;
+}
