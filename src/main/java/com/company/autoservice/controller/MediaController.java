103eebbebb24d38a75745ee1e15a3aacaf3821d5
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-09-22 09:39:00 +0500

    media class created with controller, repository and service and media upload and download APIs created

---
+++ src/main/java/com/company/autoservice/controller/MediaController.java
@@ -0,0 +1,79 @@
+    package com.company.autoservice.controller;
+
+    import com.company.autoservice.entity.Media;
+    import com.company.autoservice.service.media.MediaService;
+    import io.swagger.v3.oas.annotations.Operation;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.core.io.Resource;
+    import org.springframework.core.io.UrlResource;
+    import org.springframework.http.MediaType;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.multipart.MultipartFile;
+
+    import java.io.IOException;
+    import java.nio.file.Files;
+    import java.nio.file.Path;
+
+    @RestController
+    @RequestMapping("/api/v1/attachment")
+    @RequiredArgsConstructor
+    public class MediaController {
+
+        private final MediaService mediaService;
+
+        /**
+         *
+         * @param file -> Multipart request file which contains the input send from user
+         * @return UploadFileResponse which includes download url and file name
+         */
+        @Operation(
+                description = "POST endpoint to upload any files. Max file size must be 100MB",
+                summary = "API to store file"
+        )
+        @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        public Media uploadFile(
+                @RequestParam("file") MultipartFile file
+        ) {
+            return mediaService.saveFile(file);
+        }
+
+        @Operation(
+                description = "GET endpoint to download file. You need to give file id",
+                summary = "API to download file"
+        )
+        @GetMapping("/download/{fileID}")
+        public ResponseEntity<Resource> downloadFile(
+                @PathVariable Long fileID
+        ) {
+            Path file = mediaService.downloadFile(fileID);
+            try {
+                String mediaType = Files.probeContentType(file);
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(mediaType))
+                        .body(new UrlResource(file.toUri()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        /*@Operation(
+                description = "GET endpoint to download file. You need to give file id. It will send you file PDF",
+                summary = "API to download file"
+        )
+        @GetMapping("/download/{fileId}")
+        public ResponseEntity<Resource> downloadFile(@PathVariable Long fileId) {
+            Path file = mediaService.downloadFile(fileId);
+            try {
+                Resource resource = new UrlResource(file.toUri());
+
+                return ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_PDF)
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "media; filename=\"" + resource.getFilename() + "\"")
+                        .body(resource);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }*/
+
+    }
---
+++ src/main/java/com/company/autoservice/service/media/MediaService.java
@@ -0,0 +1,71 @@
+package com.company.autoservice.service.media;
+
+import com.company.autoservice.entity.Media;
+import com.company.autoservice.exception.ItemNotFoundException;
+import com.company.autoservice.repository.MediaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MediaService {
+
+    private final Path fileLocation;
+
+    @Autowired
+    private MediaRepository mediaRepository;
+
+    public MediaService() {
+        String fileUploadDir = "C:\\JAVA\\Microservice_Projects\\booking-service\\src\\main\\resources\\uploads";
+        this.fileLocation = Paths.get(fileUploadDir)
+                .toAbsolutePath().normalize();
+    }
+
+    public Media saveFile(MultipartFile file) {
+        String fullFileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        try {
+            Path targetLocation = fileLocation.resolve(fullFileName);
+            Files.copy(file.getInputStream(), targetLocation);
+        } catch (FileAlreadyExistsException e) {
+            String[] fileNameAndType = fullFileName.split("\\.");
+            fullFileName = fileNameAndType[0] + System.currentTimeMillis() + "." + fileNameAndType[1];
+            Path targetLocation = fileLocation.resolve(fullFileName);
+            try {
+                Files.copy(file.getInputStream(), targetLocation);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Media media = Media.builder()
+                .originalName(file.getOriginalFilename())
+                .fileDownloadUri(fileLocation + "\\" + file.getOriginalFilename())
+                .build();
+
+        return mediaRepository.save(media);
+    }
+
+
+    public Path downloadFile(Long fileId) {
+        Media media = getMediaById(fileId);
+        return fileLocation.resolve(media.getOriginalName());
+    }
+
+    public Media getMediaById(Long mediaID) {
+        return mediaRepository.findById(mediaID).orElseThrow(
+                () -> new ItemNotFoundException("Media not found with ID: " + mediaID)
+        );
+    }
+}
